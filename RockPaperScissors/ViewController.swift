0ef389c73c366ccb0b0abaa01b2e45a648dc2801
tubaaNur <yesiltastubanur@gmail.com>
2023-07-09 00:25:52 +0300

    Image views are centered

--- RockPaperScissors/ViewController.swift
+++ RockPaperScissors/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 class ViewController: UIViewController {
 
     
+    @IBOutlet weak var playerScore: UILabel!
+    @IBOutlet weak var computerScore: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
