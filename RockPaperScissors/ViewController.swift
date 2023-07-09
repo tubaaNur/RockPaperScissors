a34d2c3f519cf72d5ba2ab6124ec1e2f3551199d
tubaaNur <yesiltastubanur@gmail.com>
2023-07-09 15:09:03 +0300

    pass data logic and score logic added

--- RockPaperScissors/ViewController.swift
+++ RockPaperScissors/ViewController.swift
@@ -11,8 +11,8 @@ enum MovementElement: String {
     case rock, paper, scissors
     
     static var allCases: [MovementElement] {
-            return [.rock, .paper, .scissors]
-        }
+        return [.rock, .paper, .scissors]
+    }
 }
 
 enum GameResult: String {
@@ -20,104 +20,89 @@ enum GameResult: String {
 }
 
 class ViewController: UIViewController {
-    var userElement = ""
-    var computerElement = ""
-    var result = ""
-    
-    var pScore = 0
-    var cScore = 0
-    
+    var userElement: String?
+    var computerElement: String?
+    var result: String?
+    var playerScore = 0
+    var computerScore = 0
    
-    @IBOutlet weak var playerScore: UILabel!
-    @IBOutlet weak var computerScore: UILabel!
+    @IBOutlet weak var playerScoreLabel: UILabel!
+    
+    @IBOutlet weak var computerScoreLabel: UILabel!
+   
+    
     @IBAction func rockClick(_ sender: Any) {
         userElement = MovementElement.rock.rawValue
         computerElement = computerPlay()
-        
-        result = play(userElement, computerElement).rawValue
-        print(userElement + computerElement + result)
+        result = play(userElement!, computerElement!).rawValue
     }
     
     @IBAction func scissorsClick(_ sender: Any) {
         userElement = MovementElement.scissors.rawValue
         computerElement = computerPlay()
-        
-        result = play(userElement, computerElement).rawValue
-        print(userElement + computerElement + result)
+        result = play(userElement!, computerElement!).rawValue
     }
-    @IBAction func playButton(_ sender: Any) {
-    }
+    
     @IBAction func paperClick(_ sender: Any) {
-         userElement = MovementElement.paper.rawValue
+        userElement = MovementElement.paper.rawValue
         computerElement = computerPlay()
-
-        result = play(userElement, computerElement).rawValue
-        print(userElement + computerElement + result)
+        result = play(userElement!, computerElement!).rawValue
     }
-    var score = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.navigationItem.hidesBackButton = true
-        playerScore.text = "Your Score: \(pScore)"
-        computerScore.text = "Computer Score: \(cScore)"
-        // Do any additional setup after loading the view.
-      
     }
-    @IBAction func start(_ sender: Any) {
-           playerScore.text = "Your Score: 0"
-           computerScore.text = "Computer Score: 0"
-           pScore = 0
-           cScore = 0
-       }
     
     func computerPlay() -> String {
-        MovementElement.allCases.randomElement()?.rawValue ??  MovementElement.rock.rawValue
+        return MovementElement.allCases.randomElement()?.rawValue ??  MovementElement.rock.rawValue
     }
     
     func play(_ usersChoice: String, _ computersChoice: String) -> GameResult {
-        
         var gameResult = GameResult.draw
-        
         
         switch (usersChoice, computersChoice) {
         case (MovementElement.rock.rawValue, MovementElement.paper.rawValue), (MovementElement.paper.rawValue, MovementElement.scissors.rawValue), (MovementElement.scissors.rawValue, MovementElement.rock.rawValue):
-            score -= 1
+            if playerScore != 0{
+                playerScore -= 1
+            }
+            computerScore += 1
             gameResult = GameResult.lose
         case (MovementElement.paper.rawValue, MovementElement.rock.rawValue), (MovementElement.rock.rawValue, MovementElement.scissors.rawValue), (MovementElement.scissors.rawValue, MovementElement.paper.rawValue):
-            score += 1
+            if computerScore != 0{
+                computerScore -= 1
+            }
+            playerScore += 1
             gameResult = GameResult.win
         default :
             gameResult = .draw
-            
         }
+        playerScoreLabel.text = String(playerScore)
+        computerScoreLabel.text = String(computerScore)
         return gameResult
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        
-        
-        
         if(segue.identifier == "rockToResult"){
             let destinationViewController = segue.destination as! SecondScreenViewController
+            rockClick(sender)
             destinationViewController.computerElement = computerElement
             destinationViewController.userElement = userElement
             destinationViewController.result = result
         }
         if(segue.identifier == "paperToResult"){
             let destinationViewController = segue.destination as! SecondScreenViewController
+            paperClick(sender)
             destinationViewController.computerElement = computerElement
             destinationViewController.userElement = userElement
             destinationViewController.result = result
         }
-        if(segue.identifier == "scisssorsToResult"){
+        if(segue.identifier == "scissorsToResult"){
             let destinationViewController = segue.destination as! SecondScreenViewController
+            scissorsClick(sender)
             destinationViewController.computerElement = computerElement
             destinationViewController.userElement = userElement
             destinationViewController.result = result
         }
-        
     }
-    
 }
 
